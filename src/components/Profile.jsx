935bda5dfc19db9991259794311c45ca3f37a7c7
Alexis Ramos <alexisramoscaballero@gmail.com>
2021-01-06 15:40:46 -0600

    se agrega la vista desktop para el formulario de perfil

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
 const Profile = () => (
-  <div className=" flex flex-col md:flex-wrap  w-full space-y-5 py-10 px-5 mb-5 rounded-b-lg bg-rowTable">
-    <div className="flex flex-row w-full md:w-2/6">
+  <div className=" flex flex-col md:flex-row md:flex-wrap  md:space-x-4  w-full space-y-5 md:space-x-2 py-10 px-5 mb-5 rounded-b-lg bg-rowTable">
+    <div className="flex flex-row w-full"></div>
+    <div className="flex flex-row w-full md:w-3/12">
       <label
         htmlFor="FirsName"
         className="w-full md:w-44 text-sm font-medium text-gray-500"
@@ -13,13 +14,13 @@ const Profile = () => (
             type="text"
             name="FName"
             id="FName"
-            className="w-full border-none pl-2 py-2 sm:text-sm bg-search"
+            className="w-full font-roboto text-sm border-none pl-2 py-2 sm:text-sm bg-search"
             placeholder="Text"
           />
         </div>
       </label>
     </div>
-    <div className="flex flex-row w-full  md:w-2/6">
+    <div className="flex flex-row w-full md:w-3/12">
       <label
         htmlFor="Lname"
         className="w-full md:w-44 text-sm font-medium text-gray-500"
@@ -30,30 +31,30 @@ const Profile = () => (
             type="text"
             name="Lname"
             id="Lname"
-            className="w-full border-none pl-2 py-2 sm:text-sm bg-search"
+            className="w-full font-roboto text-sm border-none pl-2 py-2 sm:text-sm bg-search"
             placeholder="Text"
           />
         </div>
       </label>
     </div>
-    <div className="flex flex-row w-full">
+    <div className="flex flex-row w-full md:w-3/12">
       <label
         htmlFor="status"
         className="w-full md:w-44 text-sm font-medium text-gray-500"
       >
         Status
         <div className="w-full mt-1 relative shadow-sm">
-          <input
-            type="text"
+          <select
+            id="Status"
             name="status"
-            id="status"
-            className="w-full border-none pl-2 py-2 sm:text-sm bg-search"
-            placeholder="Text"
-          />
+            className="w-full font-roboto text-sm focus:outline-none border-none pl-2 py-2 sm:text-sm bg-search "
+          >
+            <option>Select</option>
+          </select>
         </div>
       </label>
     </div>
-    <div className="flex flex-row w-full">
+    <div className="flex flex-row w-full md:w-3/12">
       <label
         htmlFor="salary"
         className="w-full md:w-44 text-sm font-medium text-gray-500"
@@ -61,101 +62,101 @@ const Profile = () => (
         Wish Salary
         <div className="w-full mt-1 relative shadow-sm">
           <input
-            type="text"
+            type="number"
             name="salary"
             id="salary"
-            className="w-full border-none pl-2 py-2 sm:text-sm bg-search"
-            placeholder="Text"
+            className="w-full  font-roboto text-sm border-none pl-2 py-2 sm:text-sm bg-search"
+            placeholder="Number"
           />
         </div>
       </label>
     </div>
-    <div className="flex flex-row w-full">
+    <div className="flex flex-row w-full md:w-3/12">
       <label
         htmlFor="fTitle"
         className="w-full md:w-44 text-sm font-medium text-gray-500"
       >
         Functional Title
         <div className="w-full mt-1 relative shadow-sm">
-          <input
-            type="text"
-            name="fTitle"
-            id="fTitle"
-            className="w-full border-none pl-2 py-2 sm:text-sm bg-search"
-            placeholder="Text"
-          />
+          <select
+            id="Status"
+            name="status"
+            className="w-full font-roboto text-sm focus:outline-none border-none pl-2 py-2 sm:text-sm bg-search text-gray-500"
+          >
+            <option>Select</option>
+          </select>
         </div>
       </label>
     </div>
-    <div className="flex flex-row w-full">
+    <div className="flex flex-row w-full md:w-3/12">
       <label
         htmlFor="title"
         className="w-full md:w-44 text-sm font-medium text-gray-500"
       >
         Title
         <div className="w-full mt-1 relative shadow-sm">
-          <input
-            type="text"
-            name="title"
-            id="title"
-            className="w-full border-none pl-2 py-2 sm:text-sm bg-search"
-            placeholder="Text"
-          />
+          <select
+            id="Status"
+            name="status"
+            className="w-full font-roboto text-sm focus:outline-none border-none pl-2 py-2 sm:text-sm bg-search text-gray-500"
+          >
+            <option>Select</option>
+          </select>
         </div>
       </label>
     </div>
-    <div className="flex flex-row w-full">
+    <div className="flex flex-row w-full md:w-3/12">
       <label
         htmlFor="seniority"
         className="w-full md:w-44 text-sm font-medium text-gray-500"
       >
         Seniority
         <div className="w-full mt-1 relative shadow-sm">
-          <input
-            type="text"
-            name="seniority"
-            id="seniority"
-            className="w-full border-none pl-2 py-2 sm:text-sm bg-search"
-            placeholder="Text"
-          />
+          <select
+            id="Status"
+            name="status"
+            className="w-full font-roboto text-sm focus:outline-none border-none pl-2 py-2 sm:text-sm bg-search text-gray-500"
+          >
+            <option>Select</option>
+          </select>
         </div>
       </label>
     </div>
-    <div className="flex flex-row w-full">
+    <div className="flex flex-row w-full md:w-3/12">
       <label
         htmlFor="industry"
         className="w-full md:w-44 text-sm font-medium text-gray-500"
       >
         Industry
         <div className="w-full mt-1 relative shadow-sm">
-          <input
-            type="text"
-            name="industry"
-            id="industry"
-            className="w-full border-none pl-2 py-2 sm:text-sm bg-search"
-            placeholder="Text"
-          />
+          <select
+            id="Status"
+            name="status"
+            className="w-full font-roboto text-sm focus:outline-none border-none pl-2 py-2 sm:text-sm bg-search text-gray-500"
+          >
+            <option>Select</option>
+          </select>
         </div>
       </label>
     </div>
-    <div className="flex flex-row w-full">
+    <div className="flex flex-row w-full md:w-3/12">
       <label
         htmlFor="location"
         className="w-full md:w-44 text-sm font-medium text-gray-500"
       >
         Location
         <div className="w-full mt-1 relative shadow-sm">
-          <input
-            type="text"
-            name="location"
-            id="location"
-            className="w-full border-none pl-2 py-2 sm:text-sm bg-search"
-            placeholder="Text"
-          />
+          <select
+            id="Status"
+            name="status"
+            className="w-full font-roboto text-sm focus:outline-none border-none pl-2 py-2 sm:text-sm bg-search text-gray-500"
+          >
+            <option>Select</option>
+          </select>
         </div>
       </label>
     </div>
-    <div className="flex flex-row w-full">
+    <div className="flex flex-row w-full md:w-3/12">
       <label
         htmlFor="phone"
         className="w-full md:w-44 text-sm font-medium text-gray-500"
@@ -166,13 +167,13 @@ const Profile = () => (
             type="number"
             name="phone"
             id="phone"
-            className="w-full border-none pl-2 py-2 sm:text-sm bg-search"
+            className="w-full font-roboto text-sm border-none pl-2 py-2 sm:text-sm bg-search"
             placeholder="Text"
           />
         </div>
       </label>
     </div>
-    <div className="flex flex-row w-full">
+    <div className="flex flex-row w-full md:w-3/12">
       <label
         htmlFor="email"
         className="w-full md:w-44 text-sm font-medium text-gray-500"
@@ -183,60 +184,60 @@ const Profile = () => (
             type="email"
             name="email"
             id="email"
-            className="w-full border-none pl-2 py-2 sm:text-sm bg-search"
+            className="w-full font-roboto text-sm border-none pl-2 py-2 sm:text-sm bg-search"
             placeholder="Text"
           />
         </div>
       </label>
     </div>
-    <div className="flex flex-row w-full">
+    <div className="flex flex-row w-full md:w-3/12">
       <label
         htmlFor="company"
         className="w-full md:w-44 text-sm font-medium text-gray-500"
       >
         Company
         <div className="w-full mt-1 relative shadow-sm">
-          <input
-            type="text"
-            name="company"
-            id="company"
-            className="w-full border-none pl-2 py-2 sm:text-sm bg-search"
-            placeholder="Text"
-          />
+          <select
+            id="Status"
+            name="status"
+            className="w-full font-roboto text-sm focus:outline-none border-none pl-2 py-2 sm:text-sm bg-search text-gray-500"
+          >
+            <option>Select</option>
+          </select>
         </div>
       </label>
     </div>
-    <div className="flex flex-row w-full">
+    <div className="flex flex-row w-full md:w-3/12">
       <label
         htmlFor="process"
         className="w-full md:w-44  text-sm font-medium text-gray-500"
       >
         Status for Process
         <div className="w-full mt-1 relative shadow-sm">
-          <input
-            type="text"
-            name="process"
-            id="process"
-            className="w-full border-none pl-2 py-2 sm:text-sm bg-search"
-            placeholder="Text"
-          />
+          <select
+            id="Status"
+            name="status"
+            className="w-full font-roboto text-sm focus:outline-none border-none pl-2 py-2 sm:text-sm bg-search text-gray-500"
+          >
+            <option>Select</option>
+          </select>
         </div>
       </label>
     </div>
-    <div className="flex flex-row w-full">
+    <div className="flex flex-row w-full md:w-3/12">
       <label
         htmlFor="relocation"
         className="w-full md:w-44 text-sm font-medium text-gray-500"
       >
         Relocation
         <div className="w-full mt-1 relative shadow-sm">
-          <input
-            type="text"
-            name="relocation"
-            id="relocation"
-            className="w-full border-none pl-2 py-2 sm:text-sm bg-search"
-            placeholder="Text"
-          />
+          <select
+            id="Status"
+            name="status"
+            className="w-full font-roboto text-sm focus:outline-none border-none pl-2 py-2 sm:text-sm bg-search text-gray-500"
+          >
+            <option>Select</option>
+          </select>
         </div>
       </label>
     </div>
