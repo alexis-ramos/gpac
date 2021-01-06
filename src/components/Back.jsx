b26cfbe59ed80b715b88b6c2b2d3896dcb3a0679
Alexis Ramos <alexisramoscaballero@gmail.com>
2021-01-06 16:37:27 -0600

    se agrega el componente de back

---
+++ src/components/Back.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { RiArrowLeftSLine } from 'react-icons/ri';
+
+const Back = () => (
+  <div className="flex flex-row w-full h-16 bg-search">
+    <Link to="/market" className="flex flex-row items-center">
+      <RiArrowLeftSLine color="red" className="w-12 h-10" />
+      <p className="font-monse font-bold">Back</p>
+    </Link>
+  </div>
+);
+
+export default Back;
--- src/containers/Talent.jsx
+++ src/containers/Talent.jsx
@@ -1,26 +1,32 @@
 import React from 'react';
 import { RiCheckboxBlankCircleFill } from 'react-icons/ri';
 import Attachment from '../components/Attachment';
+import Back from '../components/Back';
 import Details from '../components/Details';
 import Notes from '../components/Notes';
 import PictureAndButtons from '../components/PictureAndButtons';
 
 const Talent = () => (
-  <div className="container flex flex-col md:mx-auto lg:mx-auto xl:mx-auto">
-    <div className="flex flex-row justify-between pt-11">
-      <div className="flex flex-row items-center ">
-        <span className="mr-2">
-          <RiCheckboxBlankCircleFill color="red" />
-        </span>
-        <h3 className="font-monse font-bold text-lg">New Talent</h3>
-      </div>
+  <div className="flex flex-col w-full">
+    <div className="flex flex-row w-full">
+      <Back />
     </div>
-    <div className="flex flex-col pt-10 md:flex-row md:space-x-10 lg:flex-row  lg:space-x-10 xl:flex-row xl:space-x-10">
-      <PictureAndButtons />
-      <div className="flex flex-col pt-10 w-full md:w-3/4 lg:w-3/4 xl:w-3/4">
-        <Details />
-        <Attachment />
-        <Notes />
+    <div className="container flex flex-col md:mx-auto lg:mx-auto xl:mx-auto">
+      <div className="flex flex-row justify-between pt-11">
+        <div className="flex flex-row items-center ">
+          <span className="mr-2">
+            <RiCheckboxBlankCircleFill color="red" />
+          </span>
+          <h3 className="font-monse font-bold text-lg">New Talent</h3>
+        </div>
+      </div>
+      <div className="flex flex-col pt-10 md:flex-row md:space-x-10 lg:flex-row  lg:space-x-10 xl:flex-row xl:space-x-10">
+        <PictureAndButtons />
+        <div className="flex flex-col  w-full md:w-3/4 lg:w-3/4 xl:w-3/4">
+          <Details />
+          <Attachment />
+          <Notes />
+        </div>
       </div>
     </div>
   </div>
